ed7d48ef98e8de698b626802cc1df6b766c90453
Dean-Chu <zhudi_bupt@163.com>
2018-09-16 08:47:56 +0800

    rename retro_v3s_defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP Thu Sep 13 21:39:58 CST 2018"
+#define UTS_VERSION "#1 SMP Sun Sep 16 01:33:38 CST 2018"
 #define LINUX_COMPILE_BY "dean"
 #define LINUX_COMPILE_HOST "dean-ThinkPad-X100e"
 #define LINUX_COMPILER "gcc version 6.3.1 20170404 (Linaro GCC 6.3-2017.05) "
---
+++ kernel/bounds.s
@@ -0,0 +1,116 @@
+	.arch armv7-a
+	.eabi_attribute 20, 1	@ Tag_ABI_FP_denormal
+	.eabi_attribute 21, 1	@ Tag_ABI_FP_exceptions
+	.eabi_attribute 23, 3	@ Tag_ABI_FP_number_model
+	.eabi_attribute 24, 1	@ Tag_ABI_align8_needed
+	.eabi_attribute 25, 1	@ Tag_ABI_align8_preserved
+	.eabi_attribute 26, 2	@ Tag_ABI_enum_size
+	.eabi_attribute 30, 2	@ Tag_ABI_optimization_goals
+	.eabi_attribute 34, 1	@ Tag_CPU_unaligned_access
+	.eabi_attribute 18, 4	@ Tag_ABI_PCS_wchar_t
+	.file	"bounds.c"
+@ GNU C89 (Linaro GCC 6.3-2017.05) version 6.3.1 20170404 (arm-linux-gnueabihf)
+@	compiled by GNU C version 4.8.4, GMP version 6.1.0, MPFR version 3.1.4, MPC version 1.0.3, isl version none
+@ GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+@ options passed:  -nostdinc -I ./arch/arm/include
+@ -I ./arch/arm/include/generated/uapi -I ./arch/arm/include/generated
+@ -I ./include -I ./arch/arm/include/uapi -I ./include/uapi
+@ -I ./include/generated/uapi -imultilib . -imultiarch arm-linux-gnueabihf
+@ -iprefix /opt/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/../lib/gcc/arm-linux-gnueabihf/6.3.1/
+@ -isysroot /opt/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/../arm-linux-gnueabihf/libc
+@ -D __KERNEL__ -D __LINUX_ARM_ARCH__=7 -U arm -D CC_HAVE_ASM_GOTO
+@ -D KBUILD_BASENAME="bounds" -D KBUILD_MODNAME="bounds"
+@ -isystem /opt/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/../lib/gcc/arm-linux-gnueabihf/6.3.1/include
+@ -include ./include/linux/kconfig.h -MD kernel/.bounds.s.d kernel/bounds.c
+@ -mlittle-endian -mabi=aapcs-linux -mno-thumb-interwork -mfpu=vfp -marm
+@ -march=armv7-a -mfloat-abi=soft -mtune=cortex-a9 -mtls-dialect=gnu
+@ -auxbase-strip kernel/bounds.s -O2 -Wall -Wundef -Wstrict-prototypes
+@ -Wno-trigraphs -Werror=implicit-function-declaration -Wno-format-security
+@ -Wno-frame-address -Wframe-larger-than=1024 -Wno-unused-but-set-variable
+@ -Wunused-const-variable=0 -Wdeclaration-after-statement -Wno-pointer-sign
+@ -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time
+@ -Werror=incompatible-pointer-types -std=gnu90 -fno-strict-aliasing
+@ -fno-common -fno-PIE -fno-dwarf2-cfi-asm -fno-ipa-sra -funwind-tables
+@ -fno-delete-null-pointer-checks -fno-stack-protector -fomit-frame-pointer
+@ -fno-var-tracking-assignments -fno-strict-overflow -fconserve-stack
+@ -fverbose-asm --param allow-store-data-races=0
+@ options enabled:  -faggressive-loop-optimizations -falign-jumps
+@ -falign-labels -falign-loops -fauto-inc-dec -fbranch-count-reg
+@ -fcaller-saves -fchkp-check-incomplete-type -fchkp-check-read
+@ -fchkp-check-write -fchkp-instrument-calls -fchkp-narrow-bounds
+@ -fchkp-optimize -fchkp-store-bounds -fchkp-use-static-bounds
+@ -fchkp-use-static-const-bounds -fchkp-use-wrappers
+@ -fcombine-stack-adjustments -fcompare-elim -fcprop-registers
+@ -fcrossjumping -fcse-follow-jumps -fdefer-pop -fdevirtualize
+@ -fdevirtualize-speculatively -fearly-inlining
+@ -feliminate-unused-debug-types -fexpensive-optimizations
+@ -fforward-propagate -ffunction-cse -fgcse -fgcse-lm -fgnu-runtime
+@ -fgnu-unique -fguess-branch-probability -fhoist-adjacent-loads -fident
+@ -fif-conversion -fif-conversion2 -findirect-inlining -finline
+@ -finline-atomics -finline-functions-called-once -finline-small-functions
+@ -fipa-cp -fipa-cp-alignment -fipa-icf -fipa-icf-functions
+@ -fipa-icf-variables -fipa-profile -fipa-pure-const -fipa-ra
+@ -fipa-reference -fira-hoist-pressure -fira-share-save-slots
+@ -fira-share-spill-slots -fisolate-erroneous-paths-dereference -fivopts
+@ -fkeep-static-consts -fleading-underscore -flifetime-dse -flra-remat
+@ -flto-odr-type-merging -fmath-errno -fmerge-constants
+@ -fmerge-debug-strings -fmove-loop-invariants -fomit-frame-pointer
+@ -foptimize-sibling-calls -foptimize-strlen -fpartial-inlining -fpeephole
+@ -fpeephole2 -fplt -fprefetch-loop-arrays -freg-struct-return
+@ -freorder-blocks -freorder-functions -frerun-cse-after-loop
+@ -fsched-critical-path-heuristic -fsched-dep-count-heuristic
+@ -fsched-group-heuristic -fsched-interblock -fsched-last-insn-heuristic
+@ -fsched-pressure -fsched-rank-heuristic -fsched-spec
+@ -fsched-spec-insn-heuristic -fsched-stalled-insns-dep -fschedule-insns
+@ -fschedule-insns2 -fsection-anchors -fsemantic-interposition
+@ -fshow-column -fshrink-wrap -fsigned-zeros -fsplit-ivs-in-unroller
+@ -fsplit-wide-types -fssa-backprop -fssa-phiopt -fstdarg-opt
+@ -fstrict-volatile-bitfields -fsync-libcalls -fthread-jumps
+@ -ftoplevel-reorder -ftrapping-math -ftree-bit-ccp -ftree-builtin-call-dce
+@ -ftree-ccp -ftree-ch -ftree-coalesce-vars -ftree-copy-prop -ftree-cselim
+@ -ftree-dce -ftree-dominator-opts -ftree-dse -ftree-forwprop -ftree-fre
+@ -ftree-loop-if-convert -ftree-loop-im -ftree-loop-ivcanon
+@ -ftree-loop-optimize -ftree-parallelize-loops= -ftree-phiprop -ftree-pre
+@ -ftree-pta -ftree-reassoc -ftree-scev-cprop -ftree-sink -ftree-slsr
+@ -ftree-sra -ftree-switch-conversion -ftree-tail-merge -ftree-ter
+@ -ftree-vrp -funit-at-a-time -funwind-tables -fverbose-asm
+@ -fzero-initialized-in-bss -marm -mglibc -mlittle-endian
+@ -mpic-data-is-text-relative -msched-prolog -munaligned-access
+@ -mvectorize-with-neon-quad
+
+	.text
+	.align	2
+	.global	foo
+	.syntax unified
+	.arm
+	.fpu softvfp
+	.type	foo, %function
+foo:
+	.fnstart
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	.syntax divided
+@ 18 "kernel/bounds.c" 1
+	
+->NR_PAGEFLAGS #21 __NR_PAGEFLAGS	@
+@ 0 "" 2
+@ 19 "kernel/bounds.c" 1
+	
+->MAX_NR_ZONES #3 __MAX_NR_ZONES	@
+@ 0 "" 2
+@ 21 "kernel/bounds.c" 1
+	
+->NR_CPUS_BITS #3 ilog2(CONFIG_NR_CPUS)	@
+@ 0 "" 2
+@ 23 "kernel/bounds.c" 1
+	
+->SPINLOCK_SIZE #4 sizeof(spinlock_t)	@
+@ 0 "" 2
+	.arm
+	.syntax unified
+	bx	lr	@
+	.fnend
+	.size	foo, .-foo
+	.ident	"GCC: (Linaro GCC 6.3-2017.05) 6.3.1 20170404"
+	.section	.note.GNU-stack,"",%progbits
